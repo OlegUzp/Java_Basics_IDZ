3a6cb807a2b46fd86f481bcf31510876aa92ca8d
OlegUzp <111171444+OlegUzp@users.noreply.github.com>
2023-12-12 19:31:13 +0200

    Update DeductionsHandler.java
    Додав анотацію до DeductionsHandler.java

--- DeductionsHandler.java
+++ DeductionsHandler.java
@@ -1,7 +1,12 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+==========================================================================================================================================================================
+    ПРОЕКТ СТУДЕНТА 3 КУРСУ ГРУПИ 6.1211-2пі математичного факультету Запорізького національного університету
+    Проект представлено виключно як виконання практичного завданння екзаменаційної сесії з дисципліни "Мова програмування Java" (Горбенко В.І.)
+    ПРОЕКТ Є ОСОБИСТОЮ ВЛАСНІСТЮ ТА НЕ МОЖЕ ВИКОРИСТОВУВАТИСЬ ДЛЯ ФІНАНСОВИХ ЦІЛЕЙ.
+    Дата останньої зміни 12.12.2023 19:30.
+==========================================================================================================================================================================
+*/
+// Імпорт пакетів
 import java.sql.SQLException;
 import java.lang.ClassNotFoundException;
 import java.sql.DriverManager;
@@ -12,20 +17,25 @@ import java.util.Scanner;
 import java.util.Calendar;
 import java.time.LocalDateTime;  
 import java.time.format.DateTimeFormatter;  
-//import java.util.
+// Клас операцій з відрахування студентів
 public class DeductionsHandler extends DataHandler {
     private int choice;
     private Scanner sc = new Scanner(System.in, "windows-1251");
     private ResultSet resultSet;
+    //Операції зі студентами
     public void operations()  throws SQLException, ClassNotFoundException  {
         System.out.print("1 - Вивести відрахованих студентів\n2 - Вивести відрахованих студентів за параметрами\n3 - Додати відрахування\nВибір:");
         choice = sc.nextInt();
         switch(choice) {
+                //Загальна база відрахованних студентів
             case 1: {
+                //Запит до бази даних
                 resultSet = getDeductionsData("SELECT d.Commandment_number,p.surname,p.name,p.last_name,st.Specialties_code,st.Year_accepted,d.Deductions_types_name,d.Date,d.Diploma_number FROM deductions as d INNER JOIN students as st ON d.Students_id=st.id INNER JOIN people as p ON p.id=st.People_ID;");
                 System.out.printf("| %-3s | %-15s | %-15s | %-15s | %-10s | %-15s | %-30s | %-15s | %-15s |\n",
                                      "Код", "Прізвище","Ім'я","По-батькові","Спец.","Рік вступу","Причина","Дата","Диплом");
+                //Форматоване виведення даних з отриманої множини значень
                 while(resultSet.next()) {
+                    //Виведення за ідентифікатором окрім нульового, нульовий був для початку автоматичного збільшення індексу запису
                     if(resultSet.getString(1).compareTo("0")!=0) {
                         System.out.printf("| %-3s | %-15s | %-15s | %-15s | %-10s | %-15s | %-30s | %-15s | %-15s |\n",
                     resultSet.getString(1), resultSet.getString(2),resultSet.getString(3),
@@ -35,19 +45,24 @@ public class DeductionsHandler extends DataHandler {
                 }
                 break;
             }
+            //Виведення за параметрами
             case 2: {
+                //Змінна введення та шаблону типів String
                 String value="",template = "SELECT d.Commandment_number,p.surname,p.name,p.last_name,st.Specialties_code,st.Year_accepted,d.Deductions_types_name,d.Date,d.Diploma_number FROM deductions as d INNER JOIN students as st ON d.Students_id=st.id INNER JOIN people as p ON p.id=st.People_ID WHERE ";
                 sc.nextLine();
                 System.out.print("1.Прізвище: ");
                 value = sc.nextLine();
                 if(!value.isEmpty()) {
+                    //Якщо множина не порожня, перевірка на відсутність попередніх додавань властивостей для ЗВІТУ
                     if(template.compareTo("SELECT d.Commandment_number,p.surname,p.name,p.last_name,st.Specialties_code,st.Year_accepted,d.Deductions_types_name,d.Date,d.Diploma_number FROM deductions as d INNER JOIN students as st ON d.Students_id=st.id INNER JOIN people as p ON p.id=st.People_ID WHERE ")==0){
                         template+=" p.surname='"+value+"' ";
                     }
                     else {
+                        //Якщо властивості запиту вже були додані, то просто додаю AND і власне властивість
                         template+=" AND p.surname='"+value+"' ";
                     }
                 }
+                //Подальші атрибути властивостей опрацьовуємо аналогічно першому
                 System.out.print("2.Ім'я: ");
                 value = sc.nextLine();
                 if(!value.isEmpty()) {
@@ -118,10 +133,13 @@ public class DeductionsHandler extends DataHandler {
                         template+=" AND d.Diploma_number='"+value+"' ";
                     }
                 }
+                //Закриття запиту
                 template+=';';
+                //Формування результату запиту
                 resultSet = getDeductionsData(template);
                 System.out.printf("| %-3s | %-15s | %-15s | %-15s | %-10s | %-15s | %-30s | %-15s | %-15s |\n",
                                      "Код", "Прізвище","Ім'я","По-батькові","Спец.","Рік вступу","Причина","Дата","Диплом");
+                //Форматоване виведення результату
                 while(resultSet.next()) {
                     if(resultSet.getString(1).compareTo("0")!=0) {
                         System.out.printf("| %-3s | %-15s | %-15s | %-15s | %-10s | %-15s | %-30s | %-15s | %-15s |\n",
@@ -132,11 +150,15 @@ public class DeductionsHandler extends DataHandler {
                 }
                 break;
             }
+            //Додати відрахування    
             case 3: {
-                //Додати відрахування
+                //Шаблон формування запиту додавання відрахування
                 String template = "INSERT INTO deductions (`Students_id`,`Deductions_types_name`,`Date`,`Diploma_number`) VALUES (";
+                //Перевірка на існування студента
                 String template_person_check="SELECT st.id FROM students as st INNER JOIN people as p ON p.id = st.People_ID WHERE ";
+                //Змінна для збереження результату введення
                 String value = "";
+                //Поля введення тут обов'язкові
                 System.out.print("1.Введіть прізвище: ");
                 value = sc.nextLine();
                 while(value.isEmpty()) {
@@ -159,18 +181,24 @@ public class DeductionsHandler extends DataHandler {
                     System.out.print("3.Введіть по-батькові: ");
                     value = sc.nextLine();
                 }
+                //Закриття шалону для запиту на знаходження студента
                 template_person_check+= " AND p.last_name='"+value+"';";
+                //Запит
                 resultSet = getDeductionsData(template_person_check);
+                //Перевірка існування запису на основі підрахунку кількостей записів
                 int count = 0;
+                //Змінна зберігання ідентифікатора студента
                 String user_id = "";
                 while(resultSet.next()){
                     user_id = resultSet.getString(1);
                     count++;
                 }
+                //Коли нема студента - завершення операції одразу
                 if(count==0) {
                     System.out.println("Студента не знайдено!");
                     break;
                 }
+                //Перевірка існування наказу на відрахування цього студента
                 resultSet = getDeductionsData("SELECT d.Commandment_number from deductions as d INNER JOIN students as st ON st.id=d.Students_id WHERE d.Students_id='"+user_id+"';");
                 count=0;
                 String commandment_id = "";
@@ -178,10 +206,12 @@ public class DeductionsHandler extends DataHandler {
                     commandment_id = resultSet.getString(1);
                     count++;
                 }
+                //Запис знайдено
                 if(count!=0) {
                     System.out.println("Цей студент вже відрахований наказом №"+commandment_id+"!");
                     break;
                 }
+                //Продовжуэмо формувати наказ на відрахування
                 template+="'"+user_id+"',";
                 System.out.print("4.Тип відрахування: ");
                 String type = "";
@@ -192,6 +222,7 @@ public class DeductionsHandler extends DataHandler {
                 }
                 type=value;
                 template+="'"+value+"',";
+                //Дата виставляється поточна, тобто легко підробити дату не вийде
                 template+="'"+java.time.LocalDate.now()+"'";
                 value = "";
                 if(type.compareTo("Закінчення курсу навчання")==0){
@@ -202,12 +233,15 @@ public class DeductionsHandler extends DataHandler {
                         value = sc.nextLine();
                     }
                 }
+                //Закриття формування шаблону запиту на додавання запису до бази даних
                 template += ",'"+ value + "');";
+                //Виконання сформованого вище запиту
                 statement.executeUpdate(template);
                 break;
             }
         }
     }
+    //Функція обробки запитів на отримання даних
     public ResultSet getDeductionsData(String template) throws SQLException, ClassNotFoundException {
         return statement.executeQuery(template);
     }
